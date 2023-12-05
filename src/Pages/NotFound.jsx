1503764e19ee7991814698510191a03aef934dac
rohit <rohitkedar2000@gmail.com>
2023-12-05 12:07:05 +0530

    added animation to page not found component

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -6,18 +6,21 @@ export default function NotFound() {
     <div className='hero'>
       <div className="text-center hero-content">
         <div className="max-w-lg">
-          <h1 className="text-8xl font-bold mb-8">
-            Oops!
-          </h1>
+          <div className="text-8xl font-bold mb-8 ">
+             <img className=' animate-bounce' src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404"/>
+        
+  
           <p className="text-5xl mb-8">404 - Page not found!</p>
-         <Link to='/' className="btn btn-primary btn-lg" >
-           <FaHome className='mr-2' />
+          </div>
+         <Link to='/' className="btn btn-primary btn-lg  animate-pulse" >
+           <FaHome className='mr-2 ' />
          back to home 
          </Link>
 
         </div>
       </div>
       
-    </div>
+     </div>
+  
   )
 }
